9dae823b1eedacf4b22736abe93790627fc26caa
INVICTO-RD <geraldinopenalo@gmail.com>
2025-07-07 16:54:39 -0400

    Actualizar maker-horncard.js

--- plugins/maker-horncard.js
+++ plugins/maker-horncard.js
@@ -1,10 +1,32 @@
-const handler = async (m, {conn}) => {
-  const who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender;
-  conn.sendFile(m.chat, global.API('https://some-random-api.com', '/canvas/horny', {
-    avatar: await conn.profilePictureUrl(who, 'image').catch((_) => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
-  }), 'hornycard.png', '*TÚ ESTÁS HORNY 🥵🔥*', m);
+const handler = async (m, { conn }) => {
+  // Identifica el usuario objetivo para el avatar
+  const who = m.quoted 
+    ? m.quoted.sender 
+    : (m.mentionedJid && m.mentionedJid[0]) 
+      ? m.mentionedJid[0] 
+      : m.fromMe 
+        ? conn.user.jid 
+        : m.sender;
+
+  // Intenta obtener el avatar, o usa un placeholder si falla
+  const avatarUrl = await conn.profilePictureUrl(who, 'image')
+    .catch(() => 'https://telegra.ph/file/24fa902ead26340f3df2c.png');
+
+  // Construye la URL de la API de forma directa, sin global.API
+  const apiUrl = `https://some-random-api.com/canvas/horny?avatar=${encodeURIComponent(avatarUrl)}`;
+
+  // Envía la imagen generada
+  await conn.sendFile(
+    m.chat, 
+    apiUrl, 
+    'hornycard.png', 
+    '*TÚ ESTÁS HORNY 🥵🔥*', 
+    m
+  );
 };
+
 handler.help = ['hornycard', 'hornylicense'];
 handler.tags = ['maker'];
 handler.command = /^(horny(card|license))$/i;
+
 export default handler;
